5f9ed2b110f143514a623ac6ef9472927a1a3144
Unknown <jes.librado@gmail.com>
2017-11-24 23:15:37 -0600

    Event List

--- frontend/app/controllers/event.controller.js
+++ frontend/app/controllers/event.controller.js
@@ -3,20 +3,18 @@ class Event {
         constructor(token,
                     entityKey,
                     companyKey, 
-                    email, 
-                    name, 
-                    lastName, 
-                    rfc, 
-                    phone) {
+                    date, 
+                    days, 
+                    place, 
+                    hidden) {
     
             this.token = sessionStorage.token;
             this.entityKey = entityKey;
             this.companyKey = companyKey;
-            this.email = email;
-            this.name = name;
-            this.lastName = lastName;
-            this.rfc = rfc;
-            this.phone = phone;
+            this.date = date;
+            this.days = days;
+            this.place = place;
+            this.hidden = hidden;
         }
     
         toString(){
@@ -35,211 +33,72 @@ class Event {
         };
     };
     
-    function getEvent() {
+    function listTools() {
         
-        try{
-    
-            var urlVariables = getURLVariables();
-            var customerKey = urlVariables.customerID;
-            alert("customerKey = " + customerKey);
-            var myCustomer = new Event(token = sessionStorage.token,
-                                          entityKey = customerKey);
-    
-            jQuery.ajax({
-                type: "POST",
-                url: "http://localhost:8080/_ah/api/customer_api/v1/customer/get",
-                data: myCustomer.toString(),
-                contentType: "application/json; charset=utf-8",
-                dataType: "json",
-                before: function(){
-                    
-                    // $(".msg").html("<p>Esperando respuesta...</p>");
-                },
-                success: function (response) {
-                    
-                    // $(".msg").html("<p>Message</p>");
-    
-                    $("#singleCustomer").empty();
-                    totalCustomers = response.data;
-    
-                    var myCustomer = "";
-    
-                    // Do a forEach even if the array only has one customer
-                    totalCustomers.forEach(function(customer){
-    
-                        //Place the content in the HTML
-    
-                        myCustomer +=  "<div class='hero-element'>" +
-                                            "<div class='hero-content-inner'>" +
-                                                "<form action='/editCustomer' method='GET'>" +
-                                                    "<p>" + customer.email + "</p>" + 
-                                                    "<p>" + customer.name + " " + customer.lastName + "</p>" + 
-                                                    "<p>" + customer.phone + "</p>" + 
-                                                    "<p>" + customer.rfc + "</p>" +
-                                                    "<input type='hidden' name=customerID value='" + customer.entityKey + "'/>" +
-                                                    "<input type='submit' value='Editar'/>" + 
-                                                "</form>" +
-                                            "</div>" +
-                                        "</div>"
-                    });
-    
-                    myCustomer +=   "<div class='fixed-buttons'>" + 
-                                        "<a onclick='deleteCustomer()' class='big-fixed btn-circle btn-red'><i class='fa fa-minus' aria-hidden='true'></i></a>" +
-                                    "</div>"
-    
-                    $("#singleCustomer").append(myCustomer);
-                },
-                error: function (error) {
-                    
-                    alert(error);
-                }
-            });
-        }
-        catch(error){
-          
-            alert(error);
-        }
-    }
-    
-    function getCustomerData() {
-        
-        try{
-    
-            var urlVariables = getURLVariables();
-            var customerKey = urlVariables.customerID;
-            alert("customerKey = " + customerKey);
-            var myCustomer = new Event(token = sessionStorage.token,
-                                          entityKey = customerKey);
-    
-            jQuery.ajax({
-                type: "POST",
-                url: "http://localhost:8080/_ah/api/customer_api/v1/customer/get",
-                data: myCustomer.toString(),
-                contentType: "application/json; charset=utf-8",
-                dataType: "json",
-                before: function(){
-                    
-                    // $(".msg").html("<p>Esperando respuesta...</p>");
-                },
-                success: function (response) {
-                    
-                    // $(".msg").html("<p>Message</p>");
-                    totalCustomers = response.data;
-    
-                    $("#name").empty();
-                    $("#lastName").empty();
-                    $("#email").empty();
-                    $("#rfc").empty();
-                    $("#phone").empty();
-    
-                    // Do a forEach even if the array only has one customer
-                    totalCustomers.forEach(function(customer){
-    
-                        alert(customer.toString());
-    
-                        $("#name").val(customer.name);
-                        $("#lastName").val(customer.lastName);
-                        $("#email").val(customer.email);
-                        $("#rfc").val(customer.rfc);
-                        $("#phone").val(customer.phone);
-                    });
-                },
-                error: function (error) {
-                    
-                    alert(error);
-                }
-            });
-        }
-        catch(error){
-          
-            alert(error);
-        }
-    }
-    
-    function listCustomers() {
-        
-        try{
+        try {
             
-            // alert("token : " + sessionStorage.token);
             var myData = new TokenObject();
-    
-            jQuery.ajax({
+            
+            jQuery.ajax( {
                 type: "POST",
-                url: "http://localhost:8080/_ah/api/customer_api/v1/customer/list",
+                url: "http://localhost:8080/_ah/api/tool_api/v1/tool/list",
                 data: myData.toJsonString(),
                 contentType: "application/json; charset=utf-8",
                 dataType: "json",
                 before: function(){
-                    
                     // $(".msg").html("<p>Esperando respuesta...</p>");
                 },
                 success: function (response) {
                     
                     // $(".msg").html("<p>Message</p>");
+                    $("#listTools").empty();
+                    var totalTools = response.data;
+                    var myListTools = "";
     
-                    $("#listCustomers").empty();
-                    var totalCustomers = response.data;
-    
-                    var myListCustomers = "";
-    
-                    if(totalCustomers == null){
-    
-                        myListCustomers += "<div class='hero-element'>" +
-                                                "<div class='hero-content-inner'>" +
-                                                    "<p> No hay clientes registrados </p>" + 
-                                                "</div>" +
-                                            "</div>"
+                    if(totalTools == null){
+                    
+                        myListTools +=  "<div class='hero-element'>" +
+                                            "<div class='hero-content-inner'>" +
+                                                "<p> No hay herramientas registradas </p>" + 
+                                            "</div>" +
+                                        "</div>"
                     }
     
                     else{
     
                         alert(JSON.stringify(response.data));
-    
-                        // Do a forEach even if the array only has one customer
-                        totalCustomers.forEach(function(customer){
+                        // Do a forEach even if the array only has one tool
+                        totalTools.forEach(function(tool){
                             
                             //Place the content in the HTML
-        
-                            // alert(customer);
-        
-                            myListCustomers += "<div class='hero-element'>" +
-                                                    "<div class='hero-content-inner'>" +
-                                                        "<form action='/customer' method='GET'>" +
-                                                            "<p>" + customer.email + "</p>" + 
-                                                            "<p>" + customer.name + " " + customer.lastName + "</p>" + 
-                                                            "<p>" + customer.phone + "</p>" + 
-                                                            "<p>" + customer.rfc + "</p>" +
-                                                            "<input type='hidden' name=customerID value='" + customer.entityKey + "'/>" +
-                                                            "<input type='submit' value='Ver detalle'/>" + 
-                                                        "</form>" +
-                                                    "</div>" +
-                                                "</div>"
+                            // alert(tool);
+                            tableString += "<div class='box'> \n" +
+                                            "\t<div class='box-name'>\n" +
+                                            "\t\t<p>" + tool.id + " " + tool.model +"</p>" +
+                                            "\t</div>" +
+                                            "\t<div class='box-content'>\n" +
+                                            "\t\t<p>Categoria: " + tool.category + "</p>" +
+                                            "\t\t<p>Tipo: " + tool.type + "</p>" +
+                                            "\t\t<p>Marca: " + tool.brand + "</p>" +
+                                            "\t\t<p>Costo por dia: " + tool.costPerDay + "</p>" +
+                                            "\t\t<p>Existencias: " + tool.quantity + "</p>" +
+                                            "\t\t<p>Disponibles: " + tool.available + "</p>" +
+                                            "\t\t<p>Comentarios: " + tool.comment + "</p>" +
+                                            "\t</div>" +
+                                            "</div>";
                         });
                     }
-    
-                    $("#listCustomers").append(myListCustomers);
+                
+                    $("#listEvents").append(myListTools);
                 },
                 error: function (error) {
-                    
                     alert(error);
                 }
             });
         }
         catch(error){
-          
+    
             alert(error);
         }
-    }
-    
-    function getURLVariables() {
-        
-        var vars = {};
-         
-        var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m, key, value) {
-             
-            vars[key] = value;
-        });
-     
-        return vars;
     }
     
