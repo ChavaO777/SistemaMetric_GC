ea2a9cdb4dc26314a920c10859b15caebbb00524
Unknown <neme.d@live.com>
2017-11-24 19:52:04 -0600

    tool controller list and create

--- frontend/app/controllers/tool.controller.js
+++ frontend/app/controllers/tool.controller.js
@@ -1,19 +1,49 @@
 //import {Tool} from '../models/tool.model'
-function Tool() {
-  this.id = null;
-  this.category = null;
-  this.type = null;
-  this.brand = null;
-  this.model = null;
-  this.pricePerDay = null;
-  this.quantity = null;
-  this.available = null;
-  this.comment = null;
-  this.toString() = function() {
+class Tool {
+  constructor(token,
+              entityKey,
+              companyKey,
+              category,
+              type,
+              brand,
+              model,
+              pricePerDay,
+              quantity,
+              available,
+              comment) {
+    this.token = sessionStorage.token;
+    this.entityKey = entityKey;
+    this.companyKey = companyKey;
+    this.category = category;
+    this.type = type;
+    this.brand = brand;
+    this.model = model;
+    this.pricePerDay = pricePerDay;
+    this.quantity = quantity;
+    this.available = available;
+    this.comment = comment;
+  }
+  toString() {
     return JSON.stringify(this);
   };
 }
-function create() {
+function TokenObject() {
+
+    this.token = sessionStorage.token;
+
+    this.toJsonString = function () {
+
+        return JSON.stringify(this);
+    };
+};
+function getURLVariables() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m, key, value) {
+        vars[key] = value;
+    });
+    return vars;
+}
+function createTool() {
   var tool = new Tool();
   tool.id = $('#id').val();
   tool.category = $('#category').val();
@@ -24,18 +54,20 @@ function create() {
   tool.quantity = $('#quantity').val();
   tool.available = $('#available').val();
   tool.comment = $('#comment').val();
+  alert(tool.toString());
   try{
     jQuery.ajax({
         type: "POST",
-        url: "http://localhost:8080/backend/apis/public/tool/create",
-        data: tool.toJsonString(),
+        url: "http://localhost:8080/_ah/api/tool_api/v1/tool/insert",
+        data: tool.toString(),
         contentType: "application/json; charset=utf-8",
         dataType: "json",
         before: function(){
-            $(".msg").html("<p>Esperando respuesta...</p>");
+            //$(".msg").html("<p>Esperando respuesta...</p>");
         },
         success: function (response) {
-          $(".msg").html("<p>Herramienta creado</p>");
+          alert("The tool was successfully created.");
+          window.location = "/myTools";
         },
         error: function (error) {
             alert(error);
@@ -43,75 +75,31 @@ function create() {
     });
   }
   catch(error){
-    alert(error);
+    alert('jQuery failed : '  + error);
   }
 }
-function list() {
-  var tools;
-  var tableString = "";
-  try{
-    jQuery.ajax({
-        type: "POST",
-        url: "http://localhost:8080/backend/apis/public/tool/list",
-        data: {},
-        contentType: "application/json; charset=utf-8",
-        dataType: "json",
-        before: function(){
-            $(".msg").html("<p>Esperando respuesta...</p>");
-        },
-        success: function (response) {
-          tools = response.data;
-          tools.foreach(fuction(tool) {
-            tableString += "<div class='box'> \n" +
-            "\t<div class='box-name'>\n" +
-            "\t\t<p>" + tool.id + " " + tool.model +"</p>" +
-            "\t</div>" +
-            "\t<div class='box-content'>\n" +
-            "\t\t<p>Categoria: " + tool.category + "</p>" +
-            "\t\t<p>Tipo: " + tool.type + "</p>" +
-            "\t\t<p>Marca: " + tool.brand + "</p>" +
-            "\t\t<p>Costo por dia: " + tool.costPerDay + "</p>" +
-            "\t\t<p>Existecias: " + tool.quantity + "</p>" +
-            "\t\t<p>Disponibles: " + tool.available + "</p>" +
-            "\t\t<p>Comentarios: " + tool.comment + "</p>" +
-            "\t</div>";
-          });
-          $("#listTools").append(tools);
-        },
-        error: function (error) {
-            alert(error);
-        }
-    });
-  }
-  catch(error){
-    alert(error);
-  }
-
-}
-function get() {
-  var tool;
-  var id = $("#id").val();
-  try{
-    jQuery.ajax({
-        type: "POST",
-        url: "http://localhost:8080/backend/apis/public/tool/get",
-        data: {id : id},
-        contentType: "application/json; charset=utf-8",
-        dataType: "json",
-        before: function(){
-            $(".msg").html("<p>Esperando respuesta...</p>");
-        },
-        success: function (response) {
-          var tool = new Tool();
-          tool.id = $('#id').val();
-          tool.category = $('#category').val();
-          tool.type = $('#type').val();
-          tool.brand = $('#brand').val();
-          tool.model = $('#model').val();
-          tool.pricePerDay = $('#pricePerDay').val();
-          tool.quantity = $('#quantity').val();
-          tool.available = $('#available').val();
-          tool.comment = $('#comment').val();
+function listTools() {
+  try {
+    var myData = new TokenObject();
+    jQuery.ajax( {
+      type: "POST",
+      url: "http://localhost:8080/_ah/api/tool_api/v1/tool/list",
+      data: myData.toJsonString(),
+      contentType: "application/json; charset=utf-8",
+      dataType: "json",
+      before: function(){
+        // $(".msg").html("<p>Esperando respuesta...</p>");
+      },
+      success: function (response) {
+        // $(".msg").html("<p>Message</p>");
+        $("#listTools").empty();
+        var totalTools = response.data;
+        alert(JSON.stringify(response.data));
+        var myListTools = "";
+        // Do a forEach even if the array only has one tool
+        totalTools.forEach(function(tool){
+          //Place the content in the HTML
+          // alert(tool);
           tableString += "<div class='box'> \n" +
           "\t<div class='box-name'>\n" +
           "\t\t<p>" + tool.id + " " + tool.model +"</p>" +
@@ -125,71 +113,16 @@ function get() {
           "\t\t<p>Disponibles: " + tool.available + "</p>" +
           "\t\t<p>Comentarios: " + tool.comment + "</p>" +
           "\t</div>";
-          $("#toolInfo").append(tools);
-        },
-        error: function (error) {
-            alert(error);
-        }
+          });
+          $("#listTools").append(myListTools);
+      },
+      error: function (error) {
+        alert(error);
+      }
     });
   }
   catch(error){
-    alert(error);
-  }
-}
-function update() {
-  var tool = new Tool();
-  tool.id = $('#id').val();
-  tool.category = $('#category').val();
-  tool.type = $('#type').val();
-  tool.brand = $('#brand').val();
-  tool.model = $('#model').val();
-  tool.pricePerDay = $('#pricePerDay').val();
-  tool.quantity = $('#quantity').val();
-  tool.available = $('#available').val();
-  tool.comment = $('#comment').val();
-  try{
-    jQuery.ajax({
-        type: "POST",
-        url: "http://localhost:8080/backend/apis/public/tool/update",
-        data: tool.toJsonString(),
-        contentType: "application/json; charset=utf-8",
-        dataType: "json",
-        before: function(){
-            $(".msg").html("<p>Esperando respuesta...</p>");
-        },
-        success: function (response) {
-          $(".msg").html(tool.toJsonString());
-        },
-        error: function (error) {
-            alert(error);
-        }
-    });
-  }
-  catch(error){
-    alert(error);
-  }
-}
-function delete() {
-  var id = $("#id").val();
-  try{
-    jQuery.ajax({
-        type: "POST",
-        url: "http://localhost:8080/backend/apis/public/tool/delete",
-        data: {id : id},
-        contentType: "application/json; charset=utf-8",
-        dataType: "json",
-        before: function(){
-            $(".msg").html("<p>Esperando respuesta...</p>");
-        },
-        success: function (response) {
-          $(".msg").html("Herramienta eliminado");
-        },
-        error: function (error) {
-            alert(error);
-        }
-    });
-  }
-  catch(error){
-    alert(error);
+
+      alert(error);
   }
 }
