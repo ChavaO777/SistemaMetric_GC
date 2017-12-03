762bdf917bf699628f6d4af170ab2e4181a636d3
Unknown <jes.librado@gmail.com>
2017-12-03 00:03:14 -0600

    Basic asynchronous Login with React Js

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
     let formattedData = `{"email":"${this.state.email}","password":"${this.state.password}"}`;
     console.log(formattedData);
     const xhr = new XMLHttpRequest();
-    xhr.open('post', 'http://localhost:8080/_ah/api/user_api/v1/user/login');
+    xhr.open('post', 'http://localhost:8080/_ah/api/user_api/v1/user/login', true);
     xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
     xhr.responseType = 'json';
     xhr.onloadend = ()=>{
