025578b643b06969b1f46d8a44e932f46f4e9bb2
Unknown <jes.librado@gmail.com>
2017-12-06 03:48:38 -0600

    Deleted React traces, by now

--- frontend/app/controllers/user.controller.js
+++ frontend/app/controllers/user.controller.js
@@ -42,7 +42,7 @@ function login() {
             }
             else{
                 sessionStorage.token = response.token;
-                $(".msg").html("<p>" + sessionStorage.token + "</p>");
+                $(".msg").html("<p>Adelante...</p>");
                 window.location = "/";
             }
         },
